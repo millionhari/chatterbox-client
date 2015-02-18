c3aefec0718b1e9d3cf49fbc82fce2fedec4b06f
millionhari <mikemhari@gmail.com>
2015-02-17 16:13:50 -0800

    Implement add room functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,26 +3,48 @@ var msgUsername = window.location.search.replace("?username=", "").replace("%20"
 var msgText = "";
 var msgRoom = "";
 var app = {};
+var sendRoomName;
+var dynamicData = "";
+var roomNameText = "";
 
 //JQuery
 $(document).ready(function(){
+  //On click room
+  var roomName = $('.sidebar').on('click','li', function(){
+    sendRoomName = $(this).text().replace("#","");
+    $(".chatterbox-title").empty();
+    $(".chatterbox-title").append('<a href="index.html">#chatterbox:#</a>'+sendRoomName);
+  });
+
   //On click 1
   $('.sendmessage').click(function(){
     msgText = $(".messagetext").val();
     app = {
       'username': msgUsername,
       'text': msgText,
-      'roomname': 'poopface'
+      'roomname': sendRoomName
     };
     sendMessages();
     $(".messagetext").val("");
   });
 
-  //On click lobby
-  $('.lobby').click(function(){
-    func = getRooms;
+  //Set Room
+  $('.new-room-text-button').click(function(){
+    roomNameText = $('.new-room-text').val();
+    // $(".new-room-text").empty();
+    app = {
+      'username': 'Room Guardian',
+      'text': 'Welcome to your new room!',
+      'roomname' : roomNameText
+    };
+    sendMessages();
+    $(".new-room-text").val("");
+    console.log(roomNameText);
   });
+
 });
+
+
 
 var sendMessages = function(){
   $.ajax({
@@ -42,19 +64,24 @@ var sendMessages = function(){
 };
 
 var getMessages = function(){
+  if(sendRoomName){
+    dynamicData = 'where={"roomname":"'+sendRoomName+'"}';
+  }
+
   $.ajax({
-    // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
     type: 'GET',
-    // data: {order:'-createdAt'},
-    // data: 'where={"username":"fartmonkey"}',
+    data: dynamicData,
     contentType: 'application/json',
     success: function (data) {
-      //_.each(data['results'], function(x){
       $(".chat ul").empty();
       _.each(data['results'], function(x){
-        var data = _.escape(x.username+" : "+x.text);
-        $(".chat ul").append("<li>"+ data +"</li>");
+        var userinfo = _.escape(x.username+" : "+x.text);
+        $(".chat ul").append("<li>"+ userinfo +"</li>");
+        var arrayOfRooms = $('.sidebar li').text().split("#");
+        if (arrayOfRooms.indexOf(x.roomname) === -1 && x.roomname !== undefined){
+          $('.sidebar ul').append("<li class='room'>#"+x.roomname+"</li>");
+        }
       })
     },
     error: function (data) {
@@ -64,34 +91,9 @@ var getMessages = function(){
   });
 };
 
-
-
-var getRooms = function(){
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
-    type: 'GET',
-    // data: {order:'-createdAt'},
-    data: 'where={"room": "4chan"}',
-    contentType: 'application/json',
-    success: function (data) {
-      //_.each(data['results'], function(x){
-      $(".chat ul").empty();
-      _.each(data['results'], function(x){
-        var data = _.escape(x.username+" : "+x.text);
-        $(".chat ul").append("<li>"+ data +"</li>");
-      })
-    },
-    error: function (data) {
-      console.error('chatterbox: Failed to get message');
-    }
-  });
-};
-
 //Initially set to getMessages
-var func = getMessages;
 
 getMessages();
 setInterval(function(){
-  func();
+  getMessages();
 }, 1000);
